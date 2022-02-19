fb4d8fb2bf55a9dd96639851470c469011fd144a
kshneo0 <kshneo@gmail.com>
2022-02-19 13:03:56 +0900

    sec10 ch54 Blog Service Golang Setup

--- generate.sh
+++ generate.sh
@@ -2,3 +2,4 @@
 
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:greet/greetpb/
 protoc calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:calculator/calculatorpb
+protoc blog/blogpb/blog.proto --go_out=plugins=grpc:blog/blogpb
