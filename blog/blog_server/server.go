d8729012dc48878cb2f8485c3f444c0ce7e26ca5
kshneo0 <kshneo@gmail.com>
2022-02-19 13:21:51 +0900

    sec10 ch55 MongoDB Driver Golang Setup

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -8,10 +9,22 @@ import (
 	"os/signal"
 
 	"github.com/gRpc-Go/blog/blogpb"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
+var collection *mongo.Collection
+
 type server struct {
+}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
 }
 
 func main() {
@@ -19,7 +32,19 @@ func main() {
 	// if we crash the go code, we get the file name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
+	fmt.Println("Connecting to MongoDB")
+	// connect to MongoDB
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("Blog Service Started")
+	collection = client.Database("mydb").Collection("blog")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
@@ -50,5 +75,9 @@ func main() {
 	s.Stop()
 	fmt.Println("Closing the listener")
 	lis.Close()
+	fmt.Println("Closing MongoDB Connection")
+	if err := client.Disconnect(context.TODO()); err != nil {
+		log.Fatalf("Error on disconnection with MongoDB : %v", err)
+	}
 	fmt.Println("End of Program")
 }
